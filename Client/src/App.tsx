5fe9c0e9aa1b0b68165f5b54d7e70fc6190ab675
Yuri561 <hazelirvin@gmail.com>
2025-09-16 18:18:13 -0400

    initial commit please see comments

---
+++ Client/src/App.tsx
@@ -0,0 +1,20 @@
+
+// Dear devs !! [PLEASE READ BELOW] (yp)
+
+// please be sure to comment out your updates with initials (yp)
+//stack as follows Typescript/VanillaJs/tailwindCSS/React (yp)
+// tasks will be given out weekly (yp)
+// weekly reviews before pushing to main (yp)
+//PLEASE REMOVE ALL COMMENTS UPON PROJECT COMPLEMENTION (yp)
+
+
+function App() {
+  
+  return (
+    <>
+    
+    </>
+  )
+}
+
+export default App
