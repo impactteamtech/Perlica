8dfae1fe42c9f7bb7899395b30c0618a37c16693
Yassine Ben kacem <yassinbenkacem12@gmail.com>
2025-10-16 19:10:37 +0100

    the mainAboutDev commented

--- Client/src/components/Layout/Home.tsx
+++ Client/src/components/Layout/Home.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import Hero from '../Hero/Hero';
-import SearchPackages from '../SearchPackages/SearchPackages';
 import AboutUs from '../AboutUs/AboutUs';
 import Services from '../Services/Services';
 import Reviews from '../Reviews/Reviews';
@@ -10,11 +9,7 @@ import Events from '../Events/Events';
 const Home: React.FC = () => {
     return (
         <div>
-         
             <Hero />
-            {/* <div className="absolute left-1/2 transform -translate-x-1/2 top-[700px]  z-[10]">
-             <SearchPackages />
-            </div> */}
             <AboutUs />
             <Services />
             <Events />
